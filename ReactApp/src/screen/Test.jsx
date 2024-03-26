4ebd275e67d1ddf851654abe90901a2a3a74351b
jayjOnly <andrewnicholasjansen@gmail.com>
2024-03-26 19:37:22 +0700

    OOP 0.1

--- ReactApp/src/screen/Test.jsx
+++ ReactApp/src/screen/Test.jsx
@@ -4,6 +4,7 @@ import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Modal from "react-native-modal";
 import { useIsFocused } from '@react-navigation/native';
+import { User } from '../components/OOP';
 
 
 const uploadImage = async (imageUri, imageType, imageName) => {
@@ -59,6 +60,9 @@ const AcessGallery = async () => {
 }
 
 const SplitScreen = () => {
+  const user1 = new User("user001", "Jacky", 19, "male", "jackydummy@gmail.com", "JackyTheGreat", "nandoanjing")
+  console.log(user1)
+
     const [data, setData] = useState([])
     useEffect(()=> {
         fetch("http://10.0.2.2:5000/output", {method: 'GET'}).then(
